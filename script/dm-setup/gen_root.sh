3569b70459a07b466b876efcefc107866e4db18b
Kevin Gong <dbgong@biigroup.cn>
2015-05-06 21:40:43 +0800

    fetch root zone from b.root-servers.net

--- script/dm-setup/gen_root.sh
+++ script/dm-setup/gen_root.sh
@@ -3,55 +3,65 @@
 
 script_path=`dirname $0`
 
-
-
 # load functions
 if [ -s ${script_path}/gen_root_functions.sh ]; then
-	. ${script_path}/gen_root_functions.sh
+        . ${script_path}/gen_root_functions.sh
 else
-	echo "Error: can not load gen root functions" | logger 
-	exit 1
+        echo "Error: can not load gen root functions" | logger 
+        exit 1
 fi
-
 
 # main flow
 case $1 in
   autoupdate)
     echo "$start_time" >> $logfile
+    gen_root_arpa_file
     zone_download
-    check_zone
 
     root_current_soa_serial=`head -n 1 $zone_data/root.zone |awk '{print $7}'`
-    root_origin_soa_serial=`head -n 1 $origin_data/root.zone|awk '{print $7}'`
+    root_origin_soa_serial=`sed -n 5p  $origin_data/root.zone|awk '{print $7}'`
 
     arpa_current_soa_serial=`head -n 1 $zone_data/arpa.zone |awk '{print $7}'`
-    arpa_origin_soa_serial=`head -n 1 $origin_data/arpa.zone |awk '{print $7}'`
-
-    if [ ${root_origin_soa_serial} -gt  ${root_current_soa_serial:=0}  ]; then
-      gen_root_zone
-      sign_root_zone
-      reload_bind
-    else
-      echo "root zone file was not update!!!" >> $logfile
-    fi
-
+    arpa_origin_soa_serial=`sed -n 5p $origin_data/arpa.zone |awk '{print $7}'`
+     
     if [ $arpa_origin_soa_serial -gt ${arpa_current_soa_serial:=0} ]; then
         gen_arpa_zone
         sign_arpa_zone
         reload_bind
     else
         echo "arpa zone file was not update !!!!"  >> $logfile
+        
     fi
 
-#   update_data
+    if [ ${root_origin_soa_serial} -gt  ${root_current_soa_serial:=0}  ]; then
+      gen_root_zone
+      
+      insert_arpa_ds
+      sign_root_zone
+      reload_bind
+    else
+      echo "root zone file was not update!!!" >> $logfile
+    fi
+
     ;;
-  manual_update)
-    sign_root_zone
+  manualupdate)
+   
+    gen_root_arpa_file
+    zone_download
+
+    root_current_soa_serial=`head -n 1 $zone_data/root.zone |awk '{print $7}'`
+    root_origin_soa_serial=`sed -n 5p  $origin_data/root.zone|awk '{print $7}'`
+
+    arpa_current_soa_serial=`head -n 1 $zone_data/arpa.zone |awk '{print $7}'`
+    arpa_origin_soa_serial=`sed -n 5p $origin_data/arpa.zone |awk '{print $7}'`
+    gen_root_zone
+    gen_arpa_zone 
     sign_arpa_zone
+    insert_arpa_ds
+    sign_root_zone
     reload_bind
-#    update_data
     ;;
   *)
-    echo "sh gen_root.sh  autoupdate | manual_update "
+    echo "sh gen_root.sh  autoupdate | manualupdate "
     ;;
 esac
--- script/dm-setup/setting.sh
+++ script/dm-setup/setting.sh
@@ -1,6 +1,6 @@
 #
 # zone file path for bind9, please change to you zone file dir
-ROOT_ZONE_PATH=/root/dbgong/pm_scripts/gen_root_zone/dns/named/zone
+ROOT_ZONE_PATH=/pang/dns/named/zone
 [ ! -d ${ROOT_ZONE_PATH} ] && mkdir -p ${ROOT_ZONE_PATH}
 
 # admin mail
@@ -9,22 +9,22 @@ ADMIN_MAIL="ggpang@biigroup.cn"
 # key dir for bind9
 arpakeydir=$script_path/keys/arpa
 [ ! -d $arpakeydir ] && mkdir -p $arpakeydir
-
 rootkeydir=$script_path/keys/root
 [ ! -d $rootkeydir ] && mkdir -p $rootkeydir
 
 # 
+#
 zonedir=$script_path/zone_data
-[ ! -d $zonedir ] &&  mkdir $zonedir 
+[ ! -d $zonedir ] && mkdir $zonedir
 
-origin_data=$script_path/origin_data
-[ ! -d ${origin_data} ] && mkdir ${origin_data}
+origin_data=$script_path/ori_data
+[ ! -d ${origin_data} ] && mkdir $origin_data
 
 tmp_data=$script_path/tmp_data
-[ ! -d ${tmp_data} ] &&  mkdir ${tmp_data} 
+[ ! -d ${tmp_data} ] && mkdir $tmp_data
 
 app_data=$script_path/app_data
-[ ! -d ${app_data} ] &&  mkdir ${app_data}
+[ ! -d ${app_data} ] && mkdir $app_data
 
 zone_data=$script_path/zone_data
-[ ! -d ${zone_data} ] &&  mkdir ${zone_data}
+[ ! -d ${zone_data} ] &&  mkdir $zone_data
