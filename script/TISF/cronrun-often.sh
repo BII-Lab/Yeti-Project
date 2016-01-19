43255e8e33c19f1d240f7ae74d2e9407da29ae71
Paul Vixie <vixie@tisf.net>
2016-01-19 18:38:24 +0000

    massive overhaul

--- script/TISF/cronrun-often.sh
+++ script/TISF/cronrun-often.sh
@@ -9,59 +9,75 @@ yeticonf_dm="/home/vixie/work/yeticonf/dm"
 (cd $yeticonf_dm; git pull) 2>&1 | grep -v 'Already up-to-date.'
 
 #
-# first, fetch the iana zone, and stop if it's broken or unchanged
+# first, fetch the iana zone, and stop if it's broken or too new
 #
-dig @192.5.5.241 +onesoa +nocmd +nocomments +nostats . axfr > iana-root.dns.new
-if dnssec-verify -o . iana-root.dns.new > dnssec-verify.out 2>&1; then
+dig @192.5.5.241 +onesoa +nocmd +nocomments +nostats . axfr > iana-root.dns
+if dnssec-verify -o . iana-root.dns > dnssec-verify.out 2>&1; then
 	:
 else
 	cat dnssec-verify.out
 	exit 1
 fi
-if [ ! -s iana-root.dns.new ]; then
+if [ ! -s iana-root.dns ]; then
 	echo 'zero length or missing zone file from iana?' >&2
 	exit 1
 fi
-reality=$(awk '$3 = "SOA" { print $7; exit }' iana-root.dns.new)
+reality=$(awk '$3 = "SOA" { print $7; exit }' iana-root.dns)
 policy=$(cat $yeticonf_dm/iana-start-serial.txt)
 if [ $reality -lt $policy ]; then
 	exit 0
 fi
-if [ -e iana-root.dns ]; then
-	if cmp -s iana-root.dns.new iana-root.dns; then
-		exit 0
+new_zone=1
+if [ -e iana-root.dns.old ]; then
+	if cmp -s iana-root.dns iana-root.dns.old; then
+		new_zone=0
 	fi
-	mv iana-root.dns iana-root.dns.old
 fi
-mv iana-root.dns.new iana-root.dns
-
-#
-# second, create the yeti zone based on the iana zone, and sign it
-#
-keys=$(perl scripts/yeti-mkdns.pl)
-if $? -ne 0; then
-	echo yeti-mkdns failed
-	exit 1
+if [ $new_zone -ne 0 ]; then
+	rm -f iana-root.dns.old
+	cp iana-root.dns iana-root.dns.old
 fi
 
-if dnssec-signzone -Q -R -o . yeti-root.dns $keys > dnssec-signzone.out 2>&1
-then
-	:
-else
-	cat dnssec-signzone.out
-	exit 1
-fi
-rndc -s yeti-dm reload . 2>&1 | grep -v 'zone reload up-to-date'
-
 #
-# third, remake the conf-include file (allow-transfer, also-notify)
+# second, remake the conf-include file (allow-transfer, also-notify)
 #
 if perl scripts/yeti-mkinc.pl; then
 	:
 else
-	echo yeti-mkinc failed
+	echo 'yeti-mkinc failed' >&2
 	exit 1
 fi
-rndc -s yeti-dm reconfig
+new_inc=0
+if [ -e named.yeti.inc.old ]; then
+	if cmp -s named.yeti.inc named.yeti.inc.old; then
+		new_inc=0
+	fi
+fi
+if [ $new_inc -ne 0 ]; then
+	rndc -s yeti-dm reconfig
+	rm -f named.yeti.inc.old
+	cp named.yeti.inc named.yeti.inc.old
+fi
+
+#
+# third, if new zone, create the yeti zone based on the iana zone, and sign it
+#
+if [ $new_zone -ne 0 ]; then
+	keys=$(perl scripts/yeti-mkdns.pl)
+	if [ $? -ne 0 ]; then
+		echo 'yeti-mkdns failed' >&2
+		exit 1
+	fi
+
+	if dnssec-signzone -Q -R -o . yeti-root.dns $keys \
+		> dnssec-signzone.out 2>&1
+	then
+		rndc -s yeti-dm reload . 2>&1 \
+			| grep -v 'zone reload up-to-date'
+	else
+		cat dnssec-signzone.out
+		exit 1
+	fi
+fi
 
 exit
